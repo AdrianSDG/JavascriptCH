92a844931716cc9464eea83bc8553bf55f2e1003
Adrian <sollazzoadrian@gmail.com>
2021-10-28 17:05:41 -0300

    Clase 5 Objetos

---
+++ Clase 5 Objetos/javascript/index.js
@@ -0,0 +1,44 @@
+console.log("Clase 5 Objetos");
+
+//let nombre = "Homero";
+//let edad = 39;
+//let calle = "Av. Siempreviva 742";
+// Los variables anteriores entran relacionados entre sí, entonces mejor usamos un objeto literal
+const persona1 = { nombre: "Homero", edad: 39, calle: "Av. Siempreviva 742" }
+console.log(persona1.nombre);
+console.log(persona1.edad);
+console.log(persona1.calle);
+
+const nombreProd = "Megaminator 3000";
+const precio = 500;
+const disponible = true;
+
+const producto = {};
+
+console.log(producto);
+console.log(typeof producto);
+
+producto.nombre ="Megaminator 3000";
+
+/////////////////////////////////////////////
+
+const objetoUno = {
+    nombre: "Telescope 1.5",
+    precio: 1000,
+    disponible: true,
+}
+
+console.log(objetoUno);
+console.dir(objetoUno);
+console.table(objetoUno);
+document.write("<p>"+ objetoUno.nombre +"</p>");
+document.write("<p>"+ objetoUno.precio +"</p>");
+
+if(objetoUno.disponible){
+    document.write("El "+ objetoUno.nombre +" que tanto deseas cuesta "+ objetoUno.precio +" Dolares ,¿Los tienes?");
+}else{
+    document.write("No se encuentra stock de "+ objetoUno.nombre +" por el momento");
+}
+
+
+
