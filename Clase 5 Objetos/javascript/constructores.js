f09f141bc389632dd9f894131acedcafcebab1bc
Adrian <sollazzoadrian@gmail.com>
2021-10-30 15:58:21 -0300

    Clase 5 objetos

--- Clase 5 Objetos/javascript/constructores.js
+++ Clase 5 Objetos/javascript/constructores.js
@@ -41,5 +41,51 @@ const auto = {
     }
 }
 auto.encender();
+//////////////////////////////////////////
+
+function Individuo(nombre, edad, calle) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.calle = calle;
+    this.present = function(){ console.log("HOLA SOY "+ this.nombre)}
+    }
+    const individuo1 = new Individuo("Homer", 38, "Av. Italia 1230");
+    const individuo2 = new Individuo("Margaret", 37, "Av. Italia 1230");
+    individuo1.present();
+    individuo2.present();
+////////////////////////////////////////////////
+
+const jonDoe = { nombre: "Jhon Doe", edad: 32, calle: "Av. Sarmiento 7332"};
+//devuelve true porque la clave "nombre" existe en el objeto persona1
+console.log( "nombre" in jonDoe);
+//devuelve false porque la clave "origen" no existe en el objeto persona1
+console.log( "origen" in jonDoe);
+//recorremos todas las propiedades del objeto con el ciclo for...in
+for (const propiedad in jonDoe) {
+console.log(jonDoe[propiedad]);
+}
+//////////////////////////////////////////////////////////////////////
+//CLASES
+//Las clases de javascript, introducidas en ES6, proveen una sintaxis mucho más clara y
+//simple para crear objetos personalizados.
+//Son una equivalencia al empleo de función constructora y permite definir distintos tipo
+//de métodos
+
+class People{
+    constructor(nombre, edad, calle) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.calle = calle;
+    }
+    talk(){
+        console.log("HOLA SOY "+ this.nombre);
+        }
+    }
+    const people1 = new People("Martin", 39, "Gral. Artigas");
+    people1.talk();
+
+
+
+
 
 
