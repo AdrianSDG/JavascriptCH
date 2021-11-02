554101a37a8fd285bb467183ebd936f729fd25ae
Adrian <sollazzoadrian@gmail.com>
2021-11-02 17:49:01 -0300

    Clase 6 Array Metodos

--- Clase 6 Arrays/javascript/metodos.js
+++ Clase 6 Arrays/javascript/metodos.js
@@ -1,2 +1,16 @@
 console.log("Clase 6 METODOS");
 
+const miArray = ["marca", 3 ,"palabra"];
+console.log(miArray);
+console.log( miArray.toString() ); 
+
+miArray.push('otro elemento');
+console.log(miArray.length); 
+console.log(miArray);
+
+const otroArray = ["elefante","tigre","pantera",33]
+console.log(otroArray);
+console.log(otroArray.join("---"));
+
+const arrayCombo = miArray.concat(otroArray);
+console.log(arrayCombo);
