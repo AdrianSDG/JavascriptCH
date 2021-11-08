0b0946000fb9488b1a07cd41a9429b592b7634b8
Adrian <sollazzoadrian@gmail.com>
2021-11-08 18:33:34 -0300

    Clase 7 Storage y json

--- Clase 7 Storage y json/javascript/index.js
+++ Clase 7 Storage y json/javascript/index.js
@@ -1,2 +1,24 @@
 console.log("Clase 7 Storage y json");
 
+localStorage.setItem("nombre", "logan");
+localStorage.setItem("nombre", "robert");
+localStorage.setItem("nombre", "pedro");
+//Guarda el ultimo nombre ingrsado
+
+localStorage.setItem("bienvenido", "!Hola¡");
+localStorage.setItem("esValido", true );
+localStorage.setItem("numero", 23);
+
+let mensaje = localStorage.getItem("bienvenido");
+let bandera = localStorage.getItem("esValido");
+let numero = localStorage.getItem("numero");
+
+sessionStorage.setItem("seleccionado", [1,2,3,4]);
+sessionStorage.setItem("esValido", false);
+sessionStorage.setItem("email", "info@fake.com");
+
+let lista = sessionStorage.getItem("seleccionado").split(",");
+console.log(lista);
+let listaDos = sessionStorage.getItem("esValido") =="true";
+console.log(typeof listaDos);
+
