e783aa55682a80893ff355f3b59b4b0bfec92551
Adrian <sollazzoadrian@gmail.com>
2021-11-10 19:07:43 -0300

    Clase 8 DOM

--- Clase 8 DOM/javascript/index.js
+++ Clase 8 DOM/javascript/index.js
@@ -1,2 +1,16 @@
 console.log("Clase 8 DOM");
 
+let div = document.getElementById("app");
+let parrafo = document.getElementById("parrafo1");
+console.log(div.innerHTML);
+console.log(parrafo.innerHTML);
+
+let paises = document.getElementsByClassName("paises");
+console.log(paises[0].innerHTML);
+console.log(paises[1].innerHTML);
+console.log(paises[2].innerHTML);
+
+let contenedores = document.getElementsByTagName("div");
+console.log(contenedores[0].innerHTML);
+console.log(contenedores[1].innerHTML);
+console.log(contenedores[2].innerHTML);
