ad03ccd444b4910607d596c46393104dc3e46242
Adrian <sollazzoadrian@gmail.com>
2021-11-13 19:13:33 -0300

    Clase 9 Entrega

---
+++ Clase 9 Eventos/javascript/entrega.js
@@ -0,0 +1,11 @@
+console.log("Clase 9 Entrega");
+
+const arrayCoach = [{coach: "Marcel", tarifa: 2000, estilo:"HIT y pesas", nivel: 3},
+{ coach: "Samantha", tarifa: 1200, estilo:"HIT", nivel: 1},
+{ coach: "Brandon", tarifa: 1500, estilo:"HIT y pesas", nivel: 1},
+{ coach: "Olivia", tarifa: 1700, estilo:"funcional y pesas", nivel: 2},
+{ coach: "Robert", tarifa: 1600, estilo:"HIT", nivel: 2},
+{ coach: "Max", tarifa: 1900, estilo:"funcional y pesas", nivel: 1},
+{ coach: "Michelle", tarifa: 1300, estilo:"pesas", nivel: 1},
+{ coach: "Pedro", tarifa: 1300, estilo:"pesas", nivel: 2},
+{ coach: "Gabriel", tarifa: 1600, estilo:"HIT y pesas", nivel: 2}];
