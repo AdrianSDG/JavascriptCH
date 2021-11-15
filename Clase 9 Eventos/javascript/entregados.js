4bafbe9145c9ffafc0ce39a36484b4ae6ee407e0
Adrian <sollazzoadrian@gmail.com>
2021-11-15 19:40:43 -0300

    Clase 9 Entrega Dos

--- Clase 9 Eventos/javascript/entregados.js
+++ Clase 9 Eventos/javascript/entregados.js
@@ -1,14 +1,13 @@
 console.log("Clase 9 entrega dos");
 
-let formulario = document .getElementById ("contacto");
-formulario.addEventListener ("submit", validarForm );
+let miForm = document .getElementById ("contacto");
+miForm.addEventListener ("submit", validarForm);
 function validarForm(evt){
 evt.preventDefault ();
+document.write("Por favor corrobora que tus datos son los que ingresaste");
 let formulario = evt.target;
-let seleccion = document.createElement("div");
-    seleccion.innerHTML = (formulario.children[ 0].value);
-(formulario.children[ 1].value);
-(formulario.children[ 2].value);
-(formulario.children[ 3].value);
-document.body.appendChild(seleccion);}
-
+document.write("<p>"+ formulario.children[0].value +"</p");
+document.write("<p>"+ formulario.children[1].value +"</p>");
+document.write("<p>"+ formulario.children[2].value +"</p>");
+document.write("<p>"+ formulario.children[3].value +"</p>");
+}
