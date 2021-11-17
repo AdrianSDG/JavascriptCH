ce1f948df78799c745bea19eff7a19fa2a4c0881
Adrian <sollazzoadrian@gmail.com>
2021-11-16 21:21:34 -0300

    Clase 9 Entrega Dos

--- Clase 9 Eventos/javascript/entregados.js
+++ Clase 9 Eventos/javascript/entregados.js
@@ -4,11 +4,11 @@ let miForm = document .getElementById ("contacto");
 miForm.addEventListener ("submit", validarForm);
 function validarForm(evt){
 evt.preventDefault ();
-document.write("Por favor corrobora que tus datos son los siguientes:");
+document.write("<p style='background-color: grey; font-size: 30px'>Por favor corrobora que tus datos son los siguientes: </p>");
 let formulario = evt.target;
-document.write("<p style='background-color: yellow; text-align: center'>"+ formulario.nombre.value +"</p>");
-document.write("<p style='background-color: yellow; text-align: center'>"+ formulario.apellido.value +"</p>");
-document.write("<p style='background-color: yellow; text-align: center'>"+ formulario.email.value +"</p>");
-document.write("<p style='background-color: yellow; text-align: center'>"+ formulario.telefono.value +"</p>");
-document.write("<p style='background-color: yellow; text-align: center'>Disciplina elegida: "+ formulario.grupo.value +"</p>");
+document.write("<p style='background-color: yellow; text-align: center; font-size: 20px'>"+ formulario.nombre.value +"</p>");
+document.write("<p style='background-color: yellow; text-align: center; font-size: 20px'>"+ formulario.apellido.value +"</p>");
+document.write("<p style='background-color: yellow; text-align: center; font-size: 20px'>"+ formulario.email.value +"</p>");
+document.write("<p style='background-color: yellow; text-align: center; font-size: 20px'>"+ formulario.telefono.value +"</p>");
+document.write("<p style='background-color: yellow; text-align: center; font-size: 20px'>Disciplina elegida: "+ formulario.grupo.value +"</p>");
 }
